eeb9d95006d5d6441d73a15aade6d9fdc9ba1f7d
Hirukamunasinghe <munasinghehiruka@gmail.com>
2023-10-16 14:25:24 +0530

    commit

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ cardButton.addEventListener('click',()=>{
 //Navigates to the ping pong game page after the button click
 const cardbuttonTwo = document.querySelector('.card-button-two');
 cardbuttonTwo.addEventListener('click',()=>{
-    window.location = 'ping-pong/index.html'
+    window.location = 'pingpong.html'
 })
