65b1aaf9861d34e06374af4a5cb8682ec3bd030b
Hirukamunasinghe <munasinghehiruka@gmail.com>
2023-10-16 12:05:21 +0530

    commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//Navigates to the sporting web page when the user clicks on the first read more button in the first card
+const cardButton = document.querySelector('.card-button');
+cardButton.addEventListener('click',()=>{
+    window.location = 'https://hirukamunasinghe.github.io/sporting-website-page/'
+});
+
