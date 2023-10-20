c3715b69c78a7dcd88b15db39cfbc8899d060295
Hirukamunasinghe <munasinghehiruka@gmail.com>
2023-10-20 09:02:03 +0530

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Navigates to the sporting web page when the user clicks on the first read more button in the first card
 const cardButton = document.querySelector('.card-button');
 cardButton.addEventListener('click', () => {
-    window.location = 'https://hirukamunasinghe.github.io/sporting-website-page/'
+    window.location = 'nft.html'
 });
 
 //Navigates to the ping pong game page after the button click
