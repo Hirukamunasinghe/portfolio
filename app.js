489d040841a4d18b65e1b46cef9603c67f7e7561
Hirukamunasinghe <munasinghehiruka@gmail.com>
2023-10-26 23:03:04 +0530

    commit

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ cardButtonThree.addEventListener('click', () => {
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
 //making the portfolio heading center
-const headingButton = document.querySelector('.navbar-title')
+const heading = document.querySelector('.navbar-title')
 // toggle button click
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-    headingButton.classList.add('heading-center')
+    heading.classList.add('heading-center')
 })
