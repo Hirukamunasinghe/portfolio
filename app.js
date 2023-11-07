62629ff4d54de32541c3800c623c3d1d82a30539
Hirukamunasinghe <munasinghehiruka@gmail.com>
2023-11-07 14:52:45 +0530

    commit

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-//Navigates to the sporting web page when the user clicks on the first read more button in the first card
-const cardButton = document.querySelector('.card-button');
-cardButton.addEventListener('click', () => {
-    window.location = 'nft.html'
-});
-
-//Navigates to the ping pong game page after the button click
-const cardbuttonTwo = document.querySelector('.card-button-two');
-cardbuttonTwo.addEventListener('click', () => {
-    window.location = 'pingpong.html'
-})
-
-//Navigates to the blog post after the button click
-const cardButtonThree = document.querySelector('.card-button-three');
-cardButtonThree.addEventListener('click', () => {
-    window.location = 'blog.html'
-})
-
 // accessing the toggle button
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
