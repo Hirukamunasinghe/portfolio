ccaf37576739f650b049b476dcb8b6ce70abc634
Nadun <110454669+naduncalcey@users.noreply.github.com>
2023-11-22 16:09:34 +0530

    [fix] - fix for the project card section - res/anim

--- app.js
+++ app.js
@@ -9,3 +9,39 @@ toggleButton.addEventListener('click', () => {
     heading.classList.add('heading-center')
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+    const projectCards = document.querySelectorAll(".project-card");
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= window.innerHeight &&
+            rect.right <= window.innerWidth
+        );
+    }
+
+    function handleScroll() {
+        projectCards.forEach((card) => {
+            if (isInViewport(card)) {
+                card.classList.add("animate-slide-in");
+            }
+        });
+    }
+
+    function handleResize() {
+        projectCards.forEach((card) => {
+            if (isInViewport(card)) {
+                card.classList.add("animate-slide-in");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    handleScroll(); // Initial check on page load
+});
+
+
